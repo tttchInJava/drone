e089314cc1130498033a0aa05b310a5716ac7fc9
tianch <tianch@dingdao.com>
2021-09-17 14:57:04 +0800

    test

--- src/main/java/cloud/controller/PWAController.java
+++ src/main/java/cloud/controller/PWAController.java
@@ -2,9 +2,12 @@ package cloud.controller;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author TianCH
@@ -13,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class PWAController {
+    @Value("${str.all}")
+    private String allStr;
     @ApiOperation(value = "获取所有pwa应用")
     @GetMapping("/drone")
     public String pwaGetAll() {
-        System.out.println("as");
-        return "test";
+        String[] all = allStr.split(",");
+
+        int v = (int) (Math.random() * all.length);
+        System.out.println(v);
+        return all[v];
     }
 }
